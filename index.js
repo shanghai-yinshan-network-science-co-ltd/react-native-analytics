92c83267de2928925046334713c947d90617e83c
cleverdou <lostaloneesk@126.com>
2020-06-18 14:04:12 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import {setBuried,setClickPageInfo} from './src/clickBuried'
 import {getCurrentPageId,createAnalyticsAppContainer} from './src/pageBuried'
-import {openLog,clearUserId,setUserId} from './src/nativeModule'
+import {openLog,clearUserId,setUserId,saveBusinessEvent} from './src/nativeModule'
 import  './src/editBuried'
 import {NetworkLogger} from './src/network/NetworkLogger'
 import {setWarning} from './src/config'
 
 
-export {openLog,clearUserId,setUserId,createAnalyticsAppContainer,getCurrentPageId,setClickPageInfo,setBuried,NetworkLogger,setWarning}
+export {openLog,clearUserId,setUserId,createAnalyticsAppContainer,getCurrentPageId,setClickPageInfo,setBuried,NetworkLogger,setWarning,saveBusinessEvent}
