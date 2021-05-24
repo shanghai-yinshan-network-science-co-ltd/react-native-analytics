d2bf5eda3e867a26498cfe6be330966bd8d6fad4
cleverdou <lostaloneesk@126.com>
2021-05-24 17:06:35 +0800

    适配0.64

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import {
   createTextInput,
   createHookTouchable,
-  openLog,clearUserId,setUserId,useAnalyticsScreen,getCurrentPageId,setClickPageInfo,setBuried,NetworkLogger,setWarning,saveBusinessEvent,clickEvent} from './analytics'
+  openLog,clearUserId,setUserId,useAnalyticsScreen,getCurrentPageId,setBuried,NetworkLogger,setWarning,saveBusinessEvent,clickEvent} from './analytics'
 
 export {
   createTextInput,
   createHookTouchable,
-  openLog,clearUserId,setUserId,useAnalyticsScreen,getCurrentPageId,setClickPageInfo,setBuried,NetworkLogger,setWarning,saveBusinessEvent,clickEvent};
+  openLog,clearUserId,setUserId,useAnalyticsScreen,getCurrentPageId,setBuried,NetworkLogger,setWarning,saveBusinessEvent,clickEvent};
 export default {
   createTextInput,
   createHookTouchable,
-  openLog,clearUserId,setUserId,useAnalyticsScreen,getCurrentPageId,setClickPageInfo,setBuried,NetworkLogger,setWarning,saveBusinessEvent,clickEvent};
+  openLog,clearUserId,setUserId,useAnalyticsScreen,getCurrentPageId,setBuried,NetworkLogger,setWarning,saveBusinessEvent,clickEvent};
